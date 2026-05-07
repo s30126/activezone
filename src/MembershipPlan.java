6d402784ae3d1387c28b87d44ca341b2dced5595
Lena Lewkowicz <s30126@pjwstk.edu.pl>
2026-05-08 01:27:59 +0200

    dodanie interfejsów oraz klasy MembershipPlan

---
+++ src/MembershipPlan.java
@@ -0,0 +1,68 @@
+public abstract class MembershipPlan implements Billable {
+
+    private String planCode;
+    private String clientName;
+    private int months;
+    private double baseMonthlyFee;
+    private boolean autoRenew;
+
+    public MembershipPlan(String planCode, String clientName, int months, double baseMonthlyFee, boolean autoRenew) {
+        this.planCode = planCode;
+        this.clientName = clientName;
+        this.months = months;
+        this.baseMonthlyFee = baseMonthlyFee;
+        this.autoRenew = autoRenew;
+    }
+
+    public String getPlanCode() {
+        return planCode;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public int getMonths() {
+        return months;
+    }
+
+    public double getBaseMonthlyFee() {
+        return baseMonthlyFee;
+    }
+
+    public boolean isAutoRenew() {
+        return autoRenew;
+    }
+
+    public abstract String getPlanType();
+    public abstract double calculateMonthlyNetPrice();
+
+    public double calculateMonthlyGrossPrice() {
+        return baseMonthlyFee;
+    }
+
+    public double calculateTotalNetPrice() {
+        return calculateMonthlyNetPrice()*months;
+    }
+
+    final void printSummary() {
+        System.out.println("Klient: " + clientName +
+                ", liczba miesięcy trwania planu: " + months +
+                ", miesięczna cena netto: " + calculateMonthlyNetPrice() +
+                ", miesięczna cena brutto: " + calculateMonthlyGrossPrice() +
+                ", cena całego planu: " + calculateTotalNetPrice());
+    }
+
+
+    @Override
+    public String toString() {
+        return "MembershipPlan{" +
+                "planCode='" + planCode + '\'' +
+                ", clientName='" + clientName + '\'' +
+                ", months=" + months +
+                ", baseMonthlyFee=" + baseMonthlyFee +
+                ", autoRenew=" + autoRenew +
+                '}';
+
+    }
+}
